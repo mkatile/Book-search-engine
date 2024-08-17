01ffedd6cbcd6dcca5d9f245f133bb9cc82eb084
mkatile <mariamkatile06@gmail.com>
2024-08-16 21:57:43 -0400

    add link to serever

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://googlebooks-1-18ao.onrender.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization');
   
